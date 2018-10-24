f61d814f5761a12110220101752bc12ed0cf04f2
Denis Shadrin <deshaser@gmail.com>
2018-10-24 20:11:33 +0800

    create gh-pages build

---
+++ build/precache-manifest.ae1c6c3fbd30b352c1e0d763c490800d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "adc3e83e17d37cfa9fc2dc69f1f284f9",
+    "url": "/static/media/placeholder-user.adc3e83e.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dcea957576d40c2dfdb9",
+    "url": "/static/js/main.dcea9575.chunk.js"
+  },
+  {
+    "revision": "e1287efb034f5edbce5a",
+    "url": "/static/js/1.e1287efb.chunk.js"
+  },
+  {
+    "revision": "dcea957576d40c2dfdb9",
+    "url": "/static/css/main.895e7f5d.chunk.css"
+  },
+  {
+    "revision": "f54d6d7fb6b2d9911a8b5938f51aa403",
+    "url": "/index.html"
+  }
+];
